1526cb8f0996f7d0f69a04868eb92522d1b51350
SergioGarciaGil <sergio.cuerda@gmail.com>
2023-08-01 22:19:36 -0500

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { pipe } from 'rxjs';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Documentacion Api')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
