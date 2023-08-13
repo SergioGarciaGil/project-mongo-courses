ee3a146c2c109050b0696dd857a2bad37a782730
SergioGarciaGil <sergio.cuerda@gmail.com>
2023-08-12 23:45:42 -0500

    swagger carga imagen

--- src/videos/videos.controller.ts
+++ src/videos/videos.controller.ts
@@ -1,52 +1,33 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe, UseInterceptors, UploadedFile } from '@nestjs/common';
-import { VideosService } from './videos.service';
-import { CreateVideoDto } from './dto/create-video.dto';
-import { UpdateVideoDto } from './dto/update-video.dto';
-import { ApiConsumes, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { LoggerInterceptor } from 'src/utils/logger/logger.interceptor';
+import { Controller, Post, UseInterceptors, UploadedFile } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiOperation, ApiConsumes, ApiResponse, ApiTags, ApiBody } from '@nestjs/swagger';
+import { Express } from 'express';
 import { storage } from 'src/utils/media.handle';
 
 @ApiTags('videos')
-@UseInterceptors(LoggerInterceptor)
 @Controller('videos')
-
 export class VideosController {
-  constructor(private readonly videosService: VideosService) { }
-
-  @Post()
-  create(@Body() createVideoDto: CreateVideoDto) {
-    return this.videosService.create(createVideoDto);
-  }
-
   @Post('upload')
+  @ApiBody({
+    required: true,
+    type: "multipart/form-data",
+    schema: {
+      type: "object",
+      properties: {
+        file: {
+          type: "string",
+          format: "binary",
+        },
+      },
+    },
+  })
   @ApiOperation({ summary: 'Subir imagen' })
   @ApiConsumes('multipart/form-data')
-
-  @UseInterceptors(FileInterceptor('avatar', { storage })) // 'avatar' debe coincidir con el nombre del campo en la solicitud
+  @ApiResponse({ status: 201, description: 'Imagen cargada exitosamente' })
+  @UseInterceptors(FileInterceptor('file', { storage }))
   async handleUpload(@UploadedFile() file: Express.Multer.File) {
     console.log(file); // Esto mostrará la información del archivo en la consola
 
-  }
 
-
-  @Get()
-  findAll() {
-    return this.videosService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.videosService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateVideoDto: UpdateVideoDto) {
-    return this.videosService.update(+id, updateVideoDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.videosService.remove(+id);
   }
 }
