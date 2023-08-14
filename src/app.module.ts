86ce9a2ae9060e5b0a892deff3ca51dce174c555
SergioGarciaGil <sergio.cuerda@gmail.com>
2023-08-14 00:29:24 -0500

    DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 
 
@@ -21,6 +22,8 @@ import { UserModule } from './user/user.module';
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
+    MongooseModule.forRoot(process.env.DB_URI),
 
     CoursesModule, AuthModule, VideosModule, AwardsModule, UserModule],
 
